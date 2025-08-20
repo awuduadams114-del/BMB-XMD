01c5ef40913a441ae8798fd56e0f4b117546da71
awuduadams114-del <awuduadams114@gmail.com>
2025-08-20 14:33:13 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,10 +8,10 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 
-module.exports = {
+module.exports = {.
     session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "B.M.B-TECH",
+    OWNER_NAME: process.env.OWNER_NAME |kobby Nigel| "B.M.B-TECH",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "255767862457",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
